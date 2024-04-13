dedf52c411e224b957456a8269484104f9256702
Robson Dias <robson14br@outlook.com>
2024-04-12 21:52:24 -0300

    Aulas

---
+++ aula 11/01.js
@@ -0,0 +1,7 @@
+var vel = 61
+var limite = 60
+console.log (`Sua velocidade foi de ${vel}`)
+if(vel > limite) { //condiçao simples
+    console.log('Voce ultrapassou o limite de velocidade, MULTADO')
+}
+console.log('Dirija com cuidado, use o cinto')
---
+++ aula 11/02.js
@@ -0,0 +1,6 @@
+var pais = 'Eua'
+
+if (pais != 'Brasil') {console.log('Opa voce e gringo ne')
+
+}
+else (console.log('Voce e brazuca mano'))
