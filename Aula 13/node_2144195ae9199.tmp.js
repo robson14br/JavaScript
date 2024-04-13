4c53cea9c2b59c5005b07746206add5dc89d0624
Robson Dias <robson14br@outlook.com>
2024-04-13 19:49:45 -0300

    Create node_2144195ae9199.tmp.js

---
+++ Aula 13/node_2144195ae9199.tmp.js
@@ -0,0 +1,16 @@
+/*var oi = 1
+while (oi <= 10 ) {
+    console.log('Ola mundo')
+    oi++
+}
+*/
+/*var oi = 1
+do {
+     console.log('Ola mundo')
+}while (oi <= 10 ) 
+*/
+
+var c = 1
+do {
+    console.log(`${c} indizinhos`)
+}while (c <= 10)
